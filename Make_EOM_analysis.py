f8ca1c987f551107f69484326b5f75ac65ef1a29
James Pino <rzr993@mocs.utc.edu>
2013-06-24 13:20:01 -0400

    added full analyis script

--- Make_EOM_analysis.py
+++ Make_EOM_analysis.py
@@ -10,23 +10,23 @@ G=open('EOM_analysis_actr.sh','w')
 
 run=1 #which GA run # number you are analyzing
 for filename in LIST_1:
-    print>>F, "cd",BASE_1+filename+'Data/Selected00'+str(run)
+    print>>F, "cd "+BASE_1+filename+'Data/Selected00'+str(run)
     print>>F, "python2.7 ~/Projects/SULI/EOM_selected_models_chemicalshift.py "
     print>>F, "echo 'Chemical shift on selected models Done' "
 
     print>>F, "cd",BASE_1+filename+'Data/Selected00'+str(run)+'/Selected00'+str(run)
-    print>>F, "python2.7 ~/Projects/SULI/EOM_secstruct_select.py ../GA00"+str(run)+/best 
-    print>>F, "ptraj ~/1KBH/ANAL/1KBH1_ww.prmtop trajin_select_models'
-    print>>F,"python2.7 ~/Projects/SULI/EOM_plot_secstruct_compare.py'
+    print>>F, "python2.7 ~/Projects/SULI/EOM_secstruct_select.py ../GA00"+str(run)+'/bestcurve00'+str(run)+".txt"
+    print>>F, "ptraj ~/1KBH/ANAL/1KBH1_ww.prmtop trajin_select_models"
+    print>>F, "python2.7 ~/Projects/SULI/EOM_plot_secstruct_compare.py"
 
 for filename in LIST_2:
-    print>>F, "cd",BASE_1+filename+'Data/Selected00'+str(run)
-    print>>F, "python2.7 ~/Projects/SULI/EOM_selected_models_chemicalshift.py "
-    print>>F, "echo 'Chemical shift on selected models Done' "
+    print>>G, "cd",BASE_2+filename+'Data/Selected00'+str(run)
+    print>>G, "python2.7 ~/Projects/SULI/EOM_selected_models_chemicalshift.py "
+    print>>G, "echo 'Chemical shift on selected models Done' "
 
-    print>>F, "cd",BASE_1+filename+'Data/Selected00'+str(run)+'/Selected00'+str(run)
-    print>>F, "python2.7 ~/Projects/SULI/EOM_secstruct_select.py ../GA00"+str(run)+/best 
-    print>>F, "ptraj ~/1KBH/ANAL/1KBH1_ww.prmtop trajin_select_models'
-    print>>F,"python2.7 ~/Projects/SULI/EOM_plot_secstruct_compare.py'
+    print>>G, "cd",BASE_2+filename+'Data/Selected00'+str(run)+'/Selected00'+str(run)
+    print>>G, "python2.7 ~/Projects/SULI/EOM_secstruct_select.py ../GA00"+str(run)+"/bestcurve00"+str(run)+".txt" 
+    print>>G, "ptraj ~/1KBH/ANAL/1KBH1_ww.prmtop trajin_select_models"
+    print>>G,"python2.7 ~/Projects/SULI/EOM_plot_secstruct_compare.py"
 
 		
