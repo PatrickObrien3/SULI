88a91fe7f628495ec203159ad0a810735592498e
James Pino <rzr993@mocs.utc.edu>
2013-06-24 13:22:00 -0400

    changed EOM_prep

--- EOM_prep.py
+++ EOM_prep.py
@@ -14,24 +14,27 @@ results while renumbering the files so the EOM can function properly.
 import os
 
 directories=['/home/jms/1KBH/BOTH/p_actr1', '/home/jms/1KBH/BOTH/p_actr2']
+#directories=['/home/jms/ACTR/ACTR_1/', '/home/jms/ACTR/ACTR_2/]
+#directories=['/home/jms/1KBH/ANAL/1KBH1/', '/home/jms/1KBH/ANAL/1KBH1/']
 savedir='/home/jms/1KBH/ANAL/EOM_Complex/Models_'
+
 def renumber_for_EOM(working_directory,count_start):
-	os.chdir(str(working_directory))
-	for k in range(1,11):
-		print 'Model', str(k) ,' of 10'
-		j=count_start
-		if j==1:
-			os.system('mkdir /home/jms/1KBH/ANAL/EOM_Complex/Models_'+str(k)+'/p_actr')
-		if j==5002:	
-			j=5001
-		for i in range(k,50001,10):
-			print j
-			os.system('cp    '+str(i)+'.pdb  '+savedir+str(k)+'/p_actr/'+str(j)+'x.pdb')
-			os.system('cp    '+str(i)+'x00.int  '+savedir+str(k)+'/p_actr/'+str(j)+'x00.int')
-			os.system('cp    '+str(i)+'x00.log  '+savedir+str(k)+'/p_actr/'+str(j)+'x00.log')
-			j+=1
-		print 'Completed function run'
-	return j
+    os.chdir(str(working_directory))
+    for k in range(1,11):
+        print 'Model', str(k) ,' of 10'
+        j=count_start
+        if j==1:
+            os.system('mkdir /home/jms/1KBH/ANAL/EOM_Complex/Models_'+str(k)+'/p_actr')
+        if  ==5002:	
+            j=5001
+        for i in range(k,50001,10):
+            print j
+            os.system('cp    '+str(i)+'.pdb  '+savedir+str(k)+'/p_actr/'+str(j)+'x.pdb')
+            os.system('cp    '+str(i)+'x00.int  '+savedir+str(k)+'/p_actr/'+str(j)+'x00.int')
+            os.system('cp    '+str(i)+'x00.log  '+savedir+str(k)+'/p_actr/'+str(j)+'x00.log')
+            j+=1
+        print 'Completed function run'
+    return j
         
 start=1
 for x in directories:
